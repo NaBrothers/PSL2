ff8a4d24391f18b6df9d899ea4319039d3d48d24
navining <superokvv@gmail.com>
2022-03-11 15:04:21 +0800

    plugins directory

--- psl-server/src/main/java/com/nabrothers/psl/server/manager/PluginManager.java
+++ psl-server/src/main/java/com/nabrothers/psl/server/manager/PluginManager.java
@@ -9,9 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 @Component
 @Log4j2
@@ -20,29 +18,44 @@ public class PluginManager {
 
     private List<Plugin> plugins = new ArrayList<>();
 
+    private Set<String> pathSet = new HashSet<>();
+
+    {
+        pathSet.add("./psl-server/target/plugins");
+        pathSet.add("./target/plugins");
+    }
+
     public void init() {
         try {
-            File pluginDir = new File("./psl-server/target/plugins");
-            File[] properties = pluginDir.listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".plugin.properties"));
-            if (properties == null) {
-                log.warn("找不到plugins文件夹: " + pluginDir.getCanonicalPath());
-                return;
+            boolean hasPluginDir = false;
+            for (String path: pathSet) {
+                File pluginDir = new File(path);
+                File[] properties = pluginDir.listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".plugin.properties"));
+                if (properties == null) {
+                    continue;
+                }
+                hasPluginDir = true;
+                for (File property : properties) {
+                    Properties p = new Properties();
+                    p.load(new FileInputStream(property));
+
+                    Plugin plugin = new Plugin();
+                    plugin.setName(p.getProperty("plugin.name"));
+                    plugin.setInfo(p.getProperty("plugin.info"));
+                    plugin.setAuthor(p.getProperty("plugin.author"));
+                    plugin.setPackageName(p.getProperty("plugin.package"));
+                    plugin.setPath(property.getCanonicalPath());
+                    log.info("加载插件\n" + plugin);
+
+                    context.load(plugin.getPackageName());
+
+                    plugins.add(plugin);
+                }
             }
-            for (File property : properties) {
-                Properties p = new Properties();
-                p.load(new FileInputStream(property));
 
-                Plugin plugin = new Plugin();
-                plugin.setName(p.getProperty("plugin.name"));
-                plugin.setInfo(p.getProperty("plugin.info"));
-                plugin.setAuthor(p.getProperty("plugin.author"));
-                plugin.setPackageName(p.getProperty("plugin.package"));
-                plugin.setPath(property.getCanonicalPath());
-                log.info("加载插件\n" + plugin);
-
-                context.load(plugin.getPackageName());
-
-                plugins.add(plugin);
+            if (!hasPluginDir) {
+                log.warn("找不到plugins文件夹: " + pathSet);
+                return;
             }
         } catch (Exception e) {
             log.error("插件路径解析失败", e);
