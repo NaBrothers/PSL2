317952c235b93a22645d1b27419dddd9a9f11b43
navining <superokvv@gmail.com>
2022-08-26 02:19:25 +0800

    测试功能

--- psl-core/src/main/java/com/nabrothers/psl/core/controller/TestController.java
+++ psl-core/src/main/java/com/nabrothers/psl/core/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.nabrothers.psl.core.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.nabrothers.psl.core.utils.HttpUtils;
 import com.nabrothers.psl.sdk.annotation.Handler;
 import org.springframework.stereotype.Controller;
 
@@ -13,5 +16,20 @@ public class TestController {
     @Handler(command = "测试")
     public String echo(String arg) {
         return arg;
+    }
+
+    @Handler(command = "啥是")
+    public String whatis(String arg) {
+        String retStr = HttpUtils.doGet(String.format("https://baike.baidu.com/api/openapi/BaikeLemmaCardApi?appid=%d&bk_key=%s", 379020, arg));
+        String answer = "";
+        if (retStr != null) {
+            JSONObject ret = JSON.parseObject(retStr);
+            if (!ret.isEmpty()) {
+                answer = ret.getString("abstract");
+            } else {
+                answer = "你搁这说什么b话呢";
+            }
+        }
+        return answer;
     }
 }
