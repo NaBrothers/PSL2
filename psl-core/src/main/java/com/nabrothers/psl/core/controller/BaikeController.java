c3de46739780b491bbf49bf3a80ba30b8d88c8a0
navining <superokvv@gmail.com>
2022-08-28 17:06:24 +0800

    参数名注解

--- psl-core/src/main/java/com/nabrothers/psl/core/controller/BaikeController.java
+++ psl-core/src/main/java/com/nabrothers/psl/core/controller/BaikeController.java
@@ -4,13 +4,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.nabrothers.psl.core.utils.HttpUtils;
 import com.nabrothers.psl.sdk.annotation.Handler;
+import com.nabrothers.psl.sdk.annotation.Param;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @Handler(command = "啥是")
 public class BaikeController {
     @Handler(info = "问懂哥问题，懂哥啥都懂")
-    public String whatis(String arg) {
+    public String whatis(@Param("词条名") String arg) {
         String retStr = HttpUtils.doGet(String.format("https://baike.baidu.com/api/openapi/BaikeLemmaCardApi?appid=%d&bk_key=%s", 379020, arg));
         String answer = "";
         if (retStr != null) {
