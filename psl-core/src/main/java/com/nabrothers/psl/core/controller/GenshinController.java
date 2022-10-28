f2443dd389e71060c380798a2d646ff1294ca195
navining <superokvv@gmail.com>
2022-10-28 12:54:42 +0800

    来个原

---
+++ psl-core/src/main/java/com/nabrothers/psl/core/controller/GenshinController.java
@@ -0,0 +1,17 @@
+package com.nabrothers.psl.core.controller;
+
+import com.nabrothers.psl.core.utils.HttpUtils;
+import com.nabrothers.psl.sdk.annotation.Handler;
+import com.nabrothers.psl.sdk.message.ImageMessage;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GenshinController {
+    @Handler(command = "来个原", info = "原批诱捕器")
+    public ImageMessage getGenshin() throws Exception {
+        ImageMessage response = new ImageMessage();
+        String retStr = HttpUtils.getRedirectUrl("https://api.dujin.org/pic/yuanshen");
+        response.setUrl(retStr);
+        return response;
+    }
+}
