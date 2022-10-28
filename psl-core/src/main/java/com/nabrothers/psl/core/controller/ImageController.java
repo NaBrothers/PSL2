dd44c0b93470f5ccf213b8c2bf3ad6098096eb61
navining <superokvv@gmail.com>
2022-10-28 15:55:00 +0800

    百度图片

--- psl-core/src/main/java/com/nabrothers/psl/core/controller/ImageController.java
+++ psl-core/src/main/java/com/nabrothers/psl/core/controller/ImageController.java
@@ -18,6 +18,9 @@ public class ImageController {
         String retStr = HttpUtils.doGet("https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&word=" + arg + "&pn=" + page + "&rn=1");
         JSONObject ret = JSONObject.parseObject(retStr);
         String url = ret.getJSONArray("data").getJSONObject(0).getString("middleURL");
+        if (url == null) {
+            message.setUrl("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fww1.sinaimg.cn%2Fbmiddle%2F6af89bc8gw1f8r0u46iqtj203w03wdfo.jpg&refer=http%3A%2F%2Fwww.sina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1669535645&t=ebac5f260710146e5c5ee3d667df8180");
+        }
         String fileName = url.hashCode() + ".jpg";
         HttpUtils.download(url, "./go-cqhttp/data/images/cache/" + fileName);
         message.setUrl("cache/" + fileName);
