f41bba53754ea8b457d0c3f673ec2ef2b0e180de
navining <superokvv@gmail.com>
2022-11-02 14:53:07 +0800

    来辆车2.0

--- psl-core/src/main/java/com/nabrothers/psl/core/controller/CarController.java
+++ psl-core/src/main/java/com/nabrothers/psl/core/controller/CarController.java
@@ -6,20 +6,21 @@ import com.nabrothers.psl.sdk.annotation.Handler;
 import com.nabrothers.psl.sdk.message.ImageMessage;
 import org.springframework.stereotype.Controller;
 
+import java.util.Random;
+
 @Controller
 public class CarController {
 
     @Handler(command = "来辆车", info = "随机提车")
     public ImageMessage getCar() {
         ImageMessage message = new ImageMessage();
-        String retStr = HttpUtils.doGet("https://api.apiopen.top/api/getImages?type=car&page=0&size=1");
+        int page = new Random().nextInt(10000) + 1;
+        String retStr = HttpUtils.doGet("http://wallpaper.apc.360.cn/index.php?%20c=WallPaper&a=getAppsByCategory&cid=12&start="+page+"&count=1&from=360chrome");
         JSONObject ret = JSONObject.parseObject(retStr);
-        JSONObject obj = ret.getJSONObject("result").getJSONArray("list").getJSONObject(0);
+        JSONObject obj = ret.getJSONArray("data").getJSONObject(0);
         String url = obj.getString("url");
-        String title = obj.getString("title");
-        String fileName = url.hashCode() + ".jpg";
-        HttpUtils.download(url, "./go-cqhttp/data/images/cache/" + fileName);
-        message.setUrl("cache/" + fileName);
+        String title = obj.getString("utag");
+        message.setUrl(url);
         message.setInfo(title);
         return message;
     }
