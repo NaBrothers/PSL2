80e4df8ac442554b5fc8b0a0a25b6aad93add7f4
navining <superokvv@gmail.com>
2022-12-13 18:04:28 +0800

    better ai

--- psl-server/src/main/java/com/nabrothers/psl/server/service/impl/DefaultReplyServiceImpl.java
+++ psl-server/src/main/java/com/nabrothers/psl/server/service/impl/DefaultReplyServiceImpl.java
@@ -13,31 +13,47 @@ import java.net.URLEncoder;
 @Component
 public class DefaultReplyServiceImpl implements DefaultReplyService {
 
+//    @Override
+//    public Message getReply(String message) {
+//        SimpleMessage reply = new SimpleMessage();
+//        reply.setSupportImageMode(false);
+//        try {
+//            String retStr = HttpUtils.doGet("https://api.qingyunke.com/api.php?key=free&msg=" + URLEncoder.encode(message, "UTF-8"));
+//            String content = JSONObject.parseObject(retStr).getString("content");
+//            content = content.replaceAll("\\{br\\}", "\n");
+//            int index = -1;
+//            do {
+//                index = content.indexOf("{face:");
+//                if (index > -1) {
+//                    int i = index + 6;
+//                    for (; i < content.length(); i++) {
+//                        if (content.charAt(i) == '}') {
+//                            break;
+//                        }
+//                    }
+//                    int faceId = Integer.parseInt(content.substring(index + 6, i));
+//                    content = content.substring(0, index) + String.format(CQCode.FACE_PATTERN, faceId) + content.substring(i + 1);
+//                }
+//            } while (index > -1);
+//
+//            reply.setData(content);
+//
+//        } catch (Exception ignore) {
+//
+//        } finally {
+//            return reply;
+//        }
+//    }
+
     @Override
     public Message getReply(String message) {
         SimpleMessage reply = new SimpleMessage();
         reply.setSupportImageMode(false);
         try {
-            String retStr = HttpUtils.doGet("https://api.qingyunke.com/api.php?key=free&msg=" + URLEncoder.encode(message, "UTF-8"));
-            String content = JSONObject.parseObject(retStr).getString("content");
-            content = content.replaceAll("\\{br\\}", "\n");
-            int index = -1;
-            do {
-                index = content.indexOf("{face:");
-                if (index > -1) {
-                    int i = index + 6;
-                    for (; i < content.length(); i++) {
-                        if (content.charAt(i) == '}') {
-                            break;
-                        }
-                    }
-                    int faceId = Integer.parseInt(content.substring(index + 6, i));
-                    content = content.substring(0, index) + String.format(CQCode.FACE_PATTERN, faceId) + content.substring(i + 1);
-                }
-            } while (index > -1);
-
+            String retStr = HttpUtils.doGet("http://47.93.214.136/chatgpt-api.php?keys=" + URLEncoder.encode(message, "UTF-8"));
+            String content = JSONObject.parseObject(retStr).getJSONObject("data").getString("html");
+            content = content.replaceAll("\n\n", "\n");
             reply.setData(content);
-
         } catch (Exception ignore) {
 
         } finally {
