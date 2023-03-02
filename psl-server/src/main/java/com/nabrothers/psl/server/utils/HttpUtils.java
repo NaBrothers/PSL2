803dc5727def7a178c7c1f887843186caf66ec87
navining <superokvv@gmail.com>
2023-03-03 01:51:26 +0800

    skip ssl

--- psl-server/src/main/java/com/nabrothers/psl/server/utils/HttpUtils.java
+++ psl-server/src/main/java/com/nabrothers/psl/server/utils/HttpUtils.java
@@ -15,6 +15,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.NoConnectionReuseStrategy;
@@ -295,7 +296,7 @@ public class HttpUtils {
     public static String doPostWithProxy(String url, JSONObject body, JSONObject header) throws IOException {
         String jsonStr = body.toJSONString();
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpClient httpClient = createSSLClientDefault();
         HttpPost httpPost = new HttpPost(url);
         HttpHost proxy = new HttpHost("172.245.226.43", 8899);
 
@@ -379,5 +380,40 @@ public class HttpUtils {
             log.warn("Remove proxy error", e);
         }
         return false;
+    }
+
+    public static CloseableHttpClient createSSLClientDefault(){
+        try {
+            //SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+            // 在JSSE中，证书信任管理器类就是实现了接口X509TrustManager的类。我们可以自己实现该接口，让它信任我们指定的证书。
+            // 创建SSLContext对象，并使用我们指定的信任管理器初始化
+            //信任所有
+            X509TrustManager x509mgr = new X509TrustManager() {
+                //　　该方法检查客户端的证书，若不信任该证书则抛出异常
+                public void checkClientTrusted(X509Certificate[] xcs, String string) {
+                }
+                // 　　该方法检查服务端的证书，若不信任该证书则抛出异常
+                public void checkServerTrusted(X509Certificate[] xcs, String string) {
+                }
+                // 　返回受信任的X509证书数组。
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[] { x509mgr }, null);
+            ////创建HttpsURLConnection对象，并设置其SSLSocketFactory对象
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            //  HttpsURLConnection对象就可以正常连接HTTPS了，无论其证书是否经权威机构的验证，只要实现了接口X509TrustManager的类MyX509TrustManager信任该证书。
+            return HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 创建默认的httpClient实例.
+        return  HttpClients.createDefault();
     }
 }
