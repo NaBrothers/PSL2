dce15992cc564837300c3c99a4879061fc7d3f04
navining <superokvv@gmail.com>
2023-03-20 20:09:32 +0800

    删除无用代码

--- psl-server/src/main/java/com/nabrothers/psl/server/service/impl/DefaultReplyServiceImpl.java
+++ psl-server/src/main/java/com/nabrothers/psl/server/service/impl/DefaultReplyServiceImpl.java
@@ -27,51 +27,11 @@ public class DefaultReplyServiceImpl implements DefaultReplyService {
     @Resource
     private CacheService cacheService;
 
-//    @Override
-//    public Message getReply(String message) {
-//        SimpleMessage reply = new SimpleMessage();
-//        reply.setSupportImageMode(false);
-//        try {
-//            String retStr = HttpUtils.doGet("https://api.qingyunke.com/api.php?key=free&msg=" + URLEncoder.encode(message, "UTF-8"));
-//            String content = JSONObject.parseObject(retStr).getString("content");
-//            content = content.replaceAll("\\{br\\}", "\n");
-//            int index = -1;
-//            do {
-//                index = content.indexOf("{face:");
-//                if (index > -1) {
-//                    int i = index + 6;
-//                    for (; i < content.length(); i++) {
-//                        if (content.charAt(i) == '}') {
-//                            break;
-//                        }
-//                    }
-//                    int faceId = Integer.parseInt(content.substring(index + 6, i));
-//                    content = content.substring(0, index) + String.format(CQCode.FACE_PATTERN, faceId) + content.substring(i + 1);
-//                }
-//            } while (index > -1);
-//
-//            reply.setData(content);
-//
-//        } catch (Exception ignore) {
-//
-//        } finally {
-//            return reply;
-//        }
-//    }
-
     @Override
     public Message getReply(String message) {
         TextMessage reply = new TextMessage();
         reply.setSupportImageMode(false);
         try {
-            // String retStr = HttpUtils.doGetWithProxy("http://47.93.214.136/chatgpt-api.php?keys=" + URLEncoder.encode(message, "UTF-8"));
-            // String content = JSONObject.parseObject(retStr).getJSONObject("data").getString("html");
-            // content = content.replaceAll("\n\n", "\n");
-            // reply.setData(content);
-
-            // Double temperature = new Random().nextDouble();
-            // String prompt = String.format("$Human:%s\n$懂哥:", message);
-
             JSONObject jsonObj = new JSONObject();
             jsonObj.put("model", "gpt-3.5-turbo");
             jsonObj.put("max_tokens", 2048);
@@ -90,12 +50,6 @@ public class DefaultReplyServiceImpl implements DefaultReplyService {
             oneMessage.put("content", message);
             messages.add(oneMessage);
             jsonObj.put("messages", messages);
-            // jsonObj.put("temperature", 0.5);
-            // jsonObj.put("top_p", 1);
-            // jsonObj.put("frequency_penalty", 0);
-            // jsonObj.put("presence_penalty", 0.6);
-            //jsonObj.put("echo", true);
-            //jsonObj.put("stop", Arrays.asList("$Human:", "$懂哥:"));
 
             JSONObject header = new JSONObject();
             String API_TOKEN = cacheService.get("openai", "token");
@@ -108,7 +62,6 @@ public class DefaultReplyServiceImpl implements DefaultReplyService {
             JSONObject retMsg = choice.getJSONObject("message");
             String text = retMsg.getString("content").replaceAll("\n\n", "\n");
             reply.setData(URLDecoder.decode(text, "UTF-8"));
-            //reply.setFooter(String.format("抽象程度：%d%%", (int)(temperature*100)));
         } catch (Exception e) {
             log.error(e);
             reply.setData("啊哦，出错了: " + e.getMessage());
