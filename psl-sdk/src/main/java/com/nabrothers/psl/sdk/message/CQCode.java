d0ff68fa542d2d816da493f94ce49812b635ae3e
navining <superokvv@gmail.com>
2023-05-11 12:07:31 +0800

    今日头条

--- psl-sdk/src/main/java/com/nabrothers/psl/sdk/message/CQCode.java
+++ psl-sdk/src/main/java/com/nabrothers/psl/sdk/message/CQCode.java
@@ -7,4 +7,5 @@ public class CQCode {
     public static final String AUDIO_PATTERN = "[CQ:tts,text=%s]";
     public static final String FACE_PATTERN = "[CQ:face,id=%d]";
     public static final String REPLY_PATTERN = "[CQ:reply,id=%d]";
+    public static final String SHARE_PATTERN = "[CQ:share,url=%s,title=%s,content=%s]";
 }
